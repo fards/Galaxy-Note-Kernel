a58c169ebf49f1d27a4fdf33f548dc9e8f99fa42
fards <ian.fardoe@gmail.com>
2011-11-16 14:57:16 +0000

    merged in riversource changes, added DEFCONFIG to root, changed script and Makefile to suit my system.

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-export PATH=$PATH:/home/frank_m/android-sdk-linux_x86/linaro/gcc-4.5/android-toolchain-eabi/bin
-#export PATH=$PATH:/home/frank_m/android-sdk-linux_x86/android_source/prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin
+export PATH=$PATH:~/toolchains/4.5/android-toolchain-eabi/bin
+#export PATH=$PATH:~/toolchains/4.6/android-toolchain-eabi/bin
 export ARCH=arm
-export CROSS_COMPILE=/home/frank_m/android-sdk-linux_x86/linaro/gcc-4.5/android-toolchain-eabi/bin/arm-eabi-
+export CROSS_COMPILE=arm-eabi-
 #export CROSS_COMPILE=/home/frank_m/android-sdk-linux_x86/android_source/prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin/arm-eabi-
 
-make fm_defconfig
+#make fm_defconfig
 make -j8
 find . -name *ko -exec arm-eabi-strip --strip-debug {} \;
 find . -name *ko -exec cp -av {} ./initramfs_own/lib/modules/ \;
